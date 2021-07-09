b1c0786128c384d600a7aec6af1e2a055d27972b
Dipankar Mitra <mitra.dipankar@gmail.com>
2021-07-09 14:20:13 -0700

    Added minor scales.

--- aicomposer.R
+++ aicomposer.R
@@ -5,12 +5,15 @@
 ##    ~~~~          ~~~~~~                ~~~~~~~~
 ## 06/29/2021    Dipankar Mitra        Initial version created.
 ## 07/07/2021    Dipankar Mitra        Added functionality for Maj scales.
+## 07/09/2021    Dipanakr Mitra        Added functionality for min scales.
 ################################################################################
 
 
 ## Function to randomly generate musical chords
 ## Input params 
-##           root - Letter indicating root of scale
+##           root - Single character indicating root of scale
+##           scale - Character indicating Major or minor scale
+##           measures = Numeric count of measures (assume 1 chord per measure)
 
 chords_randomize <- function(root = "C", scale = "Maj", measures = 4)
 {
@@ -22,10 +25,9 @@ chords_randomize <- function(root = "C", scale = "Maj", measures = 4)
                         "F" = 6,"F#" = 7, "G" = 8, "G#" = 9, "A" = 10, 
                               "A#" = 11, "B" = 12)
     
-    numeric_chords <- generate_chords (measures)
+    numeric_chords <- generate_chords(measures)
     
     translate_chords(numeric_chords, scale, note_table, transpose)
-
 
 }
 
@@ -45,26 +47,51 @@ generate_chords <- function(num_measures)
 translate_chords <- function(chord_numbers, scale, chord_table, transpose)
 {
     ret_val <- vector(mode = "character", length = 1 + length(chord_numbers))
-    root_chords <- c("C Maj", "C# Maj", "D Maj", "D# Maj", "E Maj", "F Maj", 
+    root_chords_maj <- c("C Maj", "C# Maj", "D Maj", "D# Maj", "E Maj", "F Maj", 
                      "F# Maj", "G Maj", "G# Maj", "A Maj", "Bp Maj", "B Maj")
-    ret_val[1] <- root_chords[transpose]
+    root_chords_min <- c("C min", "C# min", "D min", "D# min", "E min", "F min", 
+                         "F# min", "G min", "G# min", "A min", "Bp min", "B min")
+    
+    if ((scale == "Minor") || (scale == "minor") || (scale == "min"))
+    {
+        ret_val[1] <- root_chords_min[transpose]
+        
+    } 
+    else
+    {
+        ret_val[1] <- root_chords_maj[transpose]
+    }
     
 
     for (i in 2:length(ret_val))
     {
         if ((scale == "Minor") || (scale == "minor") || (scale == "min"))
         {
-            ret_val[i] <- chord_table[chord_numbers[i-1], 2*transpose + 1]  
+            ret_val[i] <- chord_table[chord_numbers[i-1], transpose + 12]
+            if (isMinorsclaeSecondchord(chord_numbers[i-1]) )
+            {## second note could be either 7th chord or 4th minor chord
+                temp <- sample(c(7,4), size = 1)
+                ret_val[i] <- chord_table[temp, transpose+12]
+            }
+            if (isMinorscaleMinorchord(chord_numbers[i-1]))
+            {
+                ret_val[i] <- paste(ret_val[i], "min")
+            }
+            else
+            {
+                ret_val[i] <- paste(ret_val[i], "Maj")
+            }
+            
         } else
         { 
-            ret_val[i] <-  chord_table[chord_numbers[i-1], 2*transpose]
-            if (isMinorchord(chord_numbers[i-1]))
+            ret_val[i] <-  chord_table[chord_numbers[i-1], transpose]
+            if (isMajorscaleMinorchord(chord_numbers[i-1]))
             {
                 ret_val[i] <- paste(ret_val[i], "min")
             }
             else if (isSeventhchord(chord_numbers[i-1]))
             {
-                ret_val[i] <- paste(chord_table[5, 2*transpose], "7")
+                ret_val[i] <- paste(chord_table[5, transpose], "7")
             }
             else
             {
@@ -76,9 +103,9 @@ translate_chords <- function(chord_numbers, scale, chord_table, transpose)
     ret_val
 }
 
-## Helper function to check if chord should be minor
+## Helper function to check if chord should be minor in major scale
 
-isMinorchord <- function(x)
+isMajorscaleMinorchord <- function(x)
 {
     minor <- FALSE
     if ( x == 2 || x == 3 || x == 6)
@@ -88,6 +115,22 @@ isMinorchord <- function(x)
     minor
 }
 
+## Helper function to check if chord should be minor in minor scale
+isMinorscaleMinorchord <- function(x)
+{
+    minor <- FALSE
+    if ( x == 1 || x == 4 || x == 5)
+    {
+        minor <- TRUE
+    }
+    minor
+}
+
+## Helper function to check if it is 2nd chord for minor scale
+isMinorsclaeSecondchord <- function(x)
+{
+    return (x == 2)
+}
 ## Helper function to check if chord should be 7th
 
 isSeventhchord <- function(x)
